8ec222f3d8c403bfe06bc312139850732388db77
Erin Hamrick <erin.hamrick76@gmail.com>
2023-08-09 15:26:35 -0500

    accessed forecast API. Reformatted page layout.

--- assets/script1.js
+++ assets/script1.js
@@ -1,4 +1,4 @@
-const currentDate = dayjs().format("dddd, MMMM D, YYYY");
+const currentDate = dayjs().format("MMMM D, YYYY");
 const apiKey = '1c8255898de80ea443150fc125ae7091';
 
 // console.log(currentDate);
@@ -18,11 +18,14 @@ function storeCity () {
     return city;
 }
 
+
+
+
 function fetchCurrentAPI (city){
 fetch(`https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=imperial`).then((data) => {
     return data.json()
-}).then((response) =>{
-console.log(response);
+}).then((response) => {
+// console.log(response);
     // do something
 let cityName = response.name;
 let icon = response.weather[0].icon;
@@ -30,33 +33,48 @@ let temp = response.main.temp;
 let humidity = response.main.humidity;
 let wind = response.wind.speed;
 let iconURL = `https://openweathermap.org/img/wn/${icon}@2x.png`;
-console.log(cityName, iconURL, temp, humidity, wind);
+// console.log(cityName, iconURL, temp, humidity, wind);
 let resultsEl = document.getElementById('results');
 
 
-let currentEl = document.createElement('p');
-currentEl.innerHTML = "Current Conditions";
-currentEl.setAttribute('class', 'current');
+let dateEl = document.createElement('span');
+dateEl.innerHTML = currentDate;
+dateEl.setAttribute('class', 'col-2');
 
 let cityEl = document.createElement('span');
 cityEl.innerHTML = cityName;
+cityEl.setAttribute('class', 'col-2')
 
 let iconEl = document.createElement('img');
+iconEl.setAttribute('class', 'icon');
+// iconEl.setAttribute('class', 'col-2');
 iconEl.src =  iconURL;
 
 let tempEl = document.createElement('span');
+tempEl.setAttribute('class', 'col-2');
 tempEl.innerHTML = temp + '\u00B0';
 
 let humidEl = document.createElement('span');
+humidEl.setAttribute('class', 'col-2')
 humidEl.innerHTML = humidity + '% humidity'; 
 
-let windEl = document.createElement('p');
-windEl.innerHTML = 'wind speed: ' + wind + ' ' + 'mph';
+let windEl = document.createElement('span');
+windEl.setAttribute('class', 'col-2')
+windEl.innerHTML = 'Wind Speed: ' + wind + ' ' + 'mph';
 
-resultsEl.append(currentEl, cityEl, iconEl, tempEl, humidEl, windEl);
-
-
+resultsEl.append(cityEl, dateEl, iconEl, tempEl, humidEl, windEl);
 })};
+
+function fetchForecastAPI (city) {
+ fetch(`https://api.openweathermap.org/data/2.5/forecast?q=Nashville&appid=${apiKey}&units=imperial`).then((data) => {
+    return data.json();
+    }).then((response) =>{
+      console.log(response)
+    //   let date = ;
+    } ) 
+}
+
+
 
 submitBtn.addEventListener('click', () => {
     const cityName = storeCity();
