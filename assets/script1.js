f4b73101c963f98bb7f301d10c8ab279f240302f
Erin Hamrick <erin.hamrick76@gmail.com>
2023-12-21 17:30:27 -0600

    refactored styling. Added new background image. Added keyup functionality to enter button in input.

--- assets/script1.js
+++ assets/script1.js
@@ -1,28 +1,29 @@
-const currentDate = dayjs().format("MMMM D, YYYY");
-const apiKey = "1c8255898de80ea443150fc125ae7091";
+const currentDate = dayjs().format('MMMM D, YYYY');
+const apiKey = '1c8255898de80ea443150fc125ae7091';
 
-const submitBtn = document.getElementById("submitBtn");
+const submitBtn = document.getElementById('submitBtn');
+const clearBtn = document.getElementById('clear');
+const inputElement = document.getElementById('input');
 
-const clearBtn = document.getElementById("clear");
-clearBtn.addEventListener("click", () => {
+clearBtn.addEventListener('click', () => {
 	location.reload();
 });
 
 function storeCity(cityName) {
-	let key = "City";
+	let key = 'City';
 	localStorage.setItem(key, cityName);
 }
 
 function getStoredCity() {
-	let storedCity = localStorage.getItem("City");
+	let storedCity = localStorage.getItem('City');
 	if (storedCity) {
-		let buttons = document.getElementById("buttons");
-		let button = document.createElement("button");
+		let buttons = document.getElementById('buttons');
+		let button = document.createElement('button');
 		button.innerHTML = storedCity;
-		button.setAttribute("class", "cityButton");
+		button.setAttribute('class', 'cityButton');
 		buttons.append(button);
 
-		button.addEventListener("click", () => {
+		button.addEventListener('click', () => {
 			fetchCurrentAPI(storedCity);
 			fetchForecastAPI(storedCity);
 		});
@@ -38,12 +39,12 @@ function fetchCurrentAPI(city) {
 		})
 		.then((response) => {
 			console.log(response);
-			let resultsEl = document.getElementById("results");
+			let resultsEl = document.getElementById('results');
 
-			resultsEl.textContent = "";
+			resultsEl.textContent = '';
 
-			if (response.message === "city not found") {
-				alert("City not found");
+			if (response.message === 'city not found') {
+				alert('City not found');
 				return null;
 			}
 			//empty city Input box
@@ -56,33 +57,33 @@ function fetchCurrentAPI(city) {
 			let iconURL = `https://openweathermap.org/img/wn/${icon}@2x.png`;
 			// console.log(cityName, iconURL, temp, humidity, wind);
 
-			let dateEl = document.createElement("span");
+			let dateEl = document.createElement('span');
 			dateEl.innerHTML = currentDate;
-			dateEl.setAttribute("class", "col");
+			dateEl.setAttribute('class', 'col');
 
-			let cityEl = document.createElement("span");
+			let cityEl = document.createElement('span');
 			cityEl.innerHTML = cityName;
-			cityEl.setAttribute("class", "col-2, cityName");
+			cityEl.setAttribute('class', 'col-2, cityName');
 
-			let iconEl = document.createElement("img");
-			iconEl.setAttribute("class", "icon");
+			let iconEl = document.createElement('img');
+			iconEl.setAttribute('class', 'icon');
 			iconEl.src = iconURL;
 
-			let tempEl = document.createElement("span");
-			tempEl.setAttribute("class", "col-2");
-			tempEl.innerHTML = temp + "\u00B0";
+			let tempEl = document.createElement('span');
+			tempEl.setAttribute('class', 'col-2');
+			tempEl.innerHTML = temp + '\u00B0';
 
-			let humidEl = document.createElement("span");
-			humidEl.setAttribute("class", "col-2");
-			humidEl.innerHTML = humidity + "% humidity";
+			let humidEl = document.createElement('span');
+			humidEl.setAttribute('class', 'col-2');
+			humidEl.innerHTML = humidity + '% humidity';
 
-			let windEl = document.createElement("span");
-			windEl.setAttribute("class", "col-2");
-			windEl.innerHTML = "Wind Speed: " + wind + " " + "mph";
+			let windEl = document.createElement('span');
+			windEl.setAttribute('class', 'col-2');
+			windEl.innerHTML = 'Wind Speed: ' + wind + ' ' + 'mph';
 
 			resultsEl.append(cityEl, dateEl, iconEl, tempEl, humidEl, windEl);
 
-			console.log(city); 
+			console.log(city);
 			storeCity(cityName);
 			getStoredCity();
 		});
@@ -97,36 +98,36 @@ function fetchForecastAPI(city) {
 		})
 		.then((response) => {
 			console.log(response);
-			let forecastEl = document.getElementById("forecast");
-			forecastEl.innerHTML = "";
+			let forecastEl = document.getElementById('forecast');
+			forecastEl.innerHTML = '';
 			for (i = 1; i < 45; i += 8) {
-				let div = document.createElement("div");
-
+				let div = document.createElement('div');
+				console.log(response);
 				let icon = response.list[i].weather[0].icon;
 				let iconURL = `https://openweathermap.org/img/wn/${icon}@2x.png`;
-				let iconEl = document.createElement("img");
+				let iconEl = document.createElement('img');
 				iconEl.src = iconURL;
 
 				let milliseconds = response.list[i].dt_txt;
 				let date = new Date(milliseconds).toDateString();
-				let dateEl = document.createElement("p");
+				let dateEl = document.createElement('p');
 				dateEl.innerHTML = date;
 
 				let temp = response.list[i].main.temp;
-				let tempEl = document.createElement("p");
-				tempEl.innerHTML = "Temp: " + temp + "\u00B0";
+				let tempEl = document.createElement('p');
+				tempEl.innerHTML = 'Temp: ' + temp + '\u00B0';
 
 				let wind = response.list[i].wind.speed;
-				let windEl = document.createElement("p");
-				windEl.innerHTML = "Wind Speed: " + wind + " mph";
+				let windEl = document.createElement('p');
+				windEl.innerHTML = 'Wind Speed: ' + wind + ' mph';
 
 				let humid = response.list[i].main.humidity;
-				let humidEl = document.createElement("p");
-				humidEl.innerHTML = "Humidity: " + humid + "%";
+				let humidEl = document.createElement('p');
+				humidEl.innerHTML = 'Humidity: ' + humid + '%';
 
 				div.append(dateEl, iconEl, tempEl, windEl, humidEl);
 				// let forecastEl = document.getElementById("forecast");
-				div.setAttribute("class", "days");
+				div.setAttribute('class', 'days');
 				forecastEl.append(div);
 			}
 			// console.log(response);
@@ -138,10 +139,22 @@ function fetchForecastAPI(city) {
 		});
 }
 
-submitBtn.addEventListener("click", async () => {
-	let cityElement = document.getElementById("input");
+submitBtn.addEventListener('click', async (event) => {
+	event.preventDefault();
+	handleFormSubmission();
+});
+
+inputElement.addEventListener('keyup', (event) => {
+	if (event.key === 'Enter') {
+		event.preventDefault();
+		handleFormSubmission();
+	}
+});
+
+function handleFormSubmission() {
+	let cityElement = document.getElementById('input');
 	let city = cityElement.value;
 	fetchCurrentAPI(city);
 	fetchForecastAPI(city);
-	cityElement.value = "";
-});
+	cityElement.value = '';
+}
